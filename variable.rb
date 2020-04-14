e34fcdffed211fc3c94b0fee417dd21674c07e9d
Harrison Price <hprice518@gmail.com>
2020-04-14 18:59:35 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,13 +1,3 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-greeting = "Hello World"
-filter_parameter_logging
-
-greeting = "Hello World"
-puts "#{greeting}."
-filter_parameter_logging
-greeting = "Hello World"
-greeting = "#{Hello World}."
-greeting = "Hello World"
-puts greeting
-clear
+greeting = "Hell"
